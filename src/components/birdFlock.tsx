df04c25b66039785bee9814445644d92629afc0b
Dimitri Bertin <dim.bertin@gmail.com>
2025-05-06 08:14:19 +0200

    Fix window

--- src/components/birdFlock.tsx
+++ src/components/birdFlock.tsx
@@ -90,10 +90,7 @@ class BirdGeometry extends THREE.BufferGeometry {
 let camera, scene, renderer;
 let mouseX = 0, mouseY = 0;
 
-let windowHalfX = window.innerWidth / 2;
-let windowHalfY = window.innerHeight / 2;
-
-const BOUNDS = 800, BOUNDS_HALF = BOUNDS / 2;
+const BOUNDS = 800;
 
 let last = performance.now();
 
@@ -108,205 +105,211 @@ let birdUniforms;
 export default function BirdFlock() {
   const containerRef = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    if(!containerRef.current) return 
+  function init() {
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 3000 );
+    camera.position.z = 350;
 
-    function init() {
-      camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 3000 );
-      camera.position.z = 350;
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xffffff );
+    scene.fog = new THREE.Fog( 0xffffff, 100, 1000 );
 
-      scene = new THREE.Scene();
-      scene.background = new THREE.Color( 0xffffff );
-      scene.fog = new THREE.Fog( 0xffffff, 100, 1000 );
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setAnimationLoop( animate );
+    containerRef.current!.appendChild( renderer.domElement );
+    initComputeRenderer();
 
-      renderer = new THREE.WebGLRenderer();
-      renderer.setPixelRatio( window.devicePixelRatio );
-      renderer.setSize( window.innerWidth, window.innerHeight );
-      renderer.setAnimationLoop( animate );
-      containerRef.current!.appendChild( renderer.domElement );
-
-      initComputeRenderer();
-
-      containerRef.current!.style.touchAction = 'none';
-      containerRef.current!.addEventListener( 'pointermove', onPointerMove );
+    containerRef.current!.style.touchAction = 'none';
+    containerRef.current!.addEventListener( 'pointermove', onPointerMove );
 
 
-      window.addEventListener( 'resize', onWindowResize );
+    window.addEventListener( 'resize', onWindowResize );
 
-      initBirds();
-    }
+    initBirds();
+  }
 
-    function initComputeRenderer() {
-      gpuCompute = new GPUComputationRenderer( WIDTH, WIDTH, renderer );
+  function initComputeRenderer() {
+    gpuCompute = new GPUComputationRenderer( WIDTH, WIDTH, renderer );
 
-      const dtPosition = gpuCompute.createTexture();
-      const dtVelocity = gpuCompute.createTexture();
-      fillPositionTexture( dtPosition );
-      fillVelocityTexture( dtVelocity );
+    const dtPosition = gpuCompute.createTexture();
+    const dtVelocity = gpuCompute.createTexture();
+    fillPositionTexture( dtPosition );
+    fillVelocityTexture( dtVelocity );
 
-      velocityVariable = gpuCompute.addVariable( 'textureVelocity', fragmentShaderVelocity, dtVelocity );
-      positionVariable = gpuCompute.addVariable( 'texturePosition', fragmentShaderPosition, dtPosition );
+    velocityVariable = gpuCompute.addVariable( 'textureVelocity', fragmentShaderVelocity, dtVelocity );
+    positionVariable = gpuCompute.addVariable( 'texturePosition', fragmentShaderPosition, dtPosition );
 
-      gpuCompute.setVariableDependencies( velocityVariable, [ positionVariable, velocityVariable ] );
-      gpuCompute.setVariableDependencies( positionVariable, [ positionVariable, velocityVariable ] );
+    gpuCompute.setVariableDependencies( velocityVariable, [ positionVariable, velocityVariable ] );
+    gpuCompute.setVariableDependencies( positionVariable, [ positionVariable, velocityVariable ] );
 
-      positionUniforms = positionVariable.material.uniforms;
-      velocityUniforms = velocityVariable.material.uniforms;
+    positionUniforms = positionVariable.material.uniforms;
+    velocityUniforms = velocityVariable.material.uniforms;
 
-      positionUniforms[ 'time' ] = { value: 0.0 };
-      positionUniforms[ 'delta' ] = { value: 0.0 };
-      velocityUniforms[ 'time' ] = { value: 1.0 };
-      velocityUniforms[ 'delta' ] = { value: 0.0 };
-      velocityUniforms[ 'testing' ] = { value: 1.0 };
-      velocityUniforms[ 'separationDistance' ] = { value: 1.0 };
-      velocityUniforms[ 'alignmentDistance' ] = { value: 1.0 };
-      velocityUniforms[ 'cohesionDistance' ] = { value: 1.0 };
-      velocityUniforms[ 'freedomFactor' ] = { value: 1.0 };
-      velocityUniforms[ 'predator' ] = { value: new THREE.Vector3() };
-      velocityVariable.material.defines.BOUNDS = BOUNDS.toFixed( 2 );
+    positionUniforms[ 'time' ] = { value: 0.0 };
+    positionUniforms[ 'delta' ] = { value: 0.0 };
+    velocityUniforms[ 'time' ] = { value: 1.0 };
+    velocityUniforms[ 'delta' ] = { value: 0.0 };
+    velocityUniforms[ 'testing' ] = { value: 1.0 };
+    velocityUniforms[ 'separationDistance' ] = { value: 1.0 };
+    velocityUniforms[ 'alignmentDistance' ] = { value: 1.0 };
+    velocityUniforms[ 'cohesionDistance' ] = { value: 1.0 };
+    velocityUniforms[ 'freedomFactor' ] = { value: 1.0 };
+    velocityUniforms[ 'predator' ] = { value: new THREE.Vector3() };
+    velocityVariable.material.defines.BOUNDS = BOUNDS.toFixed( 2 );
 
-      velocityVariable.wrapS = THREE.RepeatWrapping;
-      velocityVariable.wrapT = THREE.RepeatWrapping;
-      positionVariable.wrapS = THREE.RepeatWrapping;
-      positionVariable.wrapT = THREE.RepeatWrapping;
+    velocityVariable.wrapS = THREE.RepeatWrapping;
+    velocityVariable.wrapT = THREE.RepeatWrapping;
+    positionVariable.wrapS = THREE.RepeatWrapping;
+    positionVariable.wrapT = THREE.RepeatWrapping;
 
-      const error = gpuCompute.init();
+    const error = gpuCompute.init();
 
-      if ( error !== null ) {
+    if ( error !== null ) {
 
-        console.error( error );
-
-      }
+      console.error( error );
 
     }
 
-    function initBirds() {
+  }
 
-      const geometry = new BirdGeometry();
+  function initBirds() {
 
-      // For Vertex and Fragment
-      birdUniforms = {
-        'color': { value: new THREE.Color( 0xff2200 ) },
-        'texturePosition': { value: null },
-        'textureVelocity': { value: null },
-        'time': { value: 1.0 },
-        'delta': { value: 0.0 }
-      };
+    const geometry = new BirdGeometry();
 
-      // THREE.ShaderMaterial
-      const material = new THREE.ShaderMaterial( {
-        uniforms: birdUniforms,
-        vertexShader: birdVS,
-        fragmentShader: birdFS,
-        side: THREE.DoubleSide
+    // For Vertex and Fragment
+    birdUniforms = {
+      'color': { value: new THREE.Color( 0xff2200 ) },
+      'texturePosition': { value: null },
+      'textureVelocity': { value: null },
+      'time': { value: 1.0 },
+      'delta': { value: 0.0 }
+    };
 
-      } );
+    // THREE.ShaderMaterial
+    const material = new THREE.ShaderMaterial( {
+      uniforms: birdUniforms,
+      vertexShader: birdVS,
+      fragmentShader: birdFS,
+      side: THREE.DoubleSide
 
-      const birdMesh = new THREE.Mesh( geometry, material );
-      birdMesh.rotation.y = Math.PI / 2;
-      birdMesh.matrixAutoUpdate = false;
-      birdMesh.updateMatrix();
+    } );
 
-      scene.add( birdMesh );
+    const birdMesh = new THREE.Mesh( geometry, material );
+    birdMesh.rotation.y = Math.PI / 2;
+    birdMesh.matrixAutoUpdate = false;
+    birdMesh.updateMatrix();
 
-    }
+    scene.add( birdMesh );
 
-    function fillPositionTexture( texture ) {
+  }
 
-      const theArray = texture.image.data;
+  function fillPositionTexture( texture ) {
 
-      for ( let k = 0, kl = theArray.length; k < kl; k += 4 ) {
+    const theArray = texture.image.data;
 
-        const x = Math.random() * (BOUNDS/2) - (BOUNDS/4);
-        const y = Math.random() * (BOUNDS/2) - (BOUNDS/4) + 200; // Centré plus haut
-        const z = Math.random() * (BOUNDS/2) - (BOUNDS/4) - 200;
+    for ( let k = 0, kl = theArray.length; k < kl; k += 4 ) {
 
-        theArray[ k + 0 ] = x;
-        theArray[ k + 1 ] = y;
-        theArray[ k + 2 ] = z;
-        theArray[ k + 3 ] = 1;
+      const x = Math.random() * (BOUNDS/2) - (BOUNDS/4);
+      const y = Math.random() * (BOUNDS/2) - (BOUNDS/4) + 200; // Centré plus haut
+      const z = Math.random() * (BOUNDS/2) - (BOUNDS/4) - 200;
 
-      }
-
-    }
-
-    function fillVelocityTexture( texture ) {
-
-      const theArray = texture.image.data;
-
-      for ( let k = 0, kl = theArray.length; k < kl; k += 4 ) {
-
-        const x = Math.random() - 0.5;
-        const y = Math.random() - 0.5;
-        const z = Math.random() - 0.5;
-
-        theArray[ k + 0 ] = x * 10;
-        theArray[ k + 1 ] = y * 10;
-        theArray[ k + 2 ] = z * 10;
-        theArray[ k + 3 ] = 1;
-
-      }
+      theArray[ k + 0 ] = x;
+      theArray[ k + 1 ] = y;
+      theArray[ k + 2 ] = z;
+      theArray[ k + 3 ] = 1;
 
     }
 
-    function onWindowResize() {
+  }
 
-      windowHalfX = window.innerWidth / 2;
-      windowHalfY = window.innerHeight / 2;
+  function fillVelocityTexture( texture ) {
 
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
+    const theArray = texture.image.data;
 
-      renderer.setSize( window.innerWidth, window.innerHeight );
+    for ( let k = 0, kl = theArray.length; k < kl; k += 4 ) {
 
-    }
+      const x = Math.random() - 0.5;
+      const y = Math.random() - 0.5;
+      const z = Math.random() - 0.5;
 
-    function onPointerMove( event ) {
-
-      if ( event.isPrimary === false ) return;
-
-      mouseX = event.clientX - windowHalfX;
-      mouseY = event.clientY - windowHalfY;
+      theArray[ k + 0 ] = x * 10;
+      theArray[ k + 1 ] = y * 10;
+      theArray[ k + 2 ] = z * 10;
+      theArray[ k + 3 ] = 1;
 
     }
 
-    function animate() {
+  }
 
-      render();
+  function onWindowResize() {
 
-    }
+    // windowHalfX = window.innerWidth / 2;
+    // windowHalfY = window.innerHeight / 2;
 
-    // Dans birdFlock.tsx, modifiez la fonction render() pour éviter que la souris
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+  }
+
+  function onPointerMove( event ) {
+
+    if ( event.isPrimary === false ) return;
+
+    const windowHalfX = window.innerWidth / 2;
+    const windowHalfY = window.innerHeight / 2;
+
+    mouseX = event.clientX - windowHalfX;
+    mouseY = event.clientY - windowHalfY;
+
+  }
+
+  function animate() {
+
+    render();
+
+  }
+
+  // Dans birdFlock.tsx, modifiez la fonction render() pour éviter que la souris
 // soit réinitialisée à 10000, 10000 après chaque rendu:
 
-    function render() {
-      const now = performance.now();
-      let delta = (now - last) / 1000;
+  function render() {
+    const now = performance.now();
+    const windowHalfX = window.innerWidth / 2;
+    const windowHalfY = window.innerHeight / 2;
 
-      if (delta > 1) delta = 1; // safety cap on large deltas
-      last = now;
+    let delta = (now - last) / 1000;
 
-      positionUniforms['time'].value = now;
-      positionUniforms['delta'].value = delta;
-      velocityUniforms['time'].value = now;
-      velocityUniforms['delta'].value = delta;
-      birdUniforms['time'].value = now;
-      birdUniforms['delta'].value = delta;
+    if (delta > 1) delta = 1; // safety cap on large deltas
+    last = now;
 
-      velocityUniforms['predator'].value.set(0.5 * mouseX / windowHalfX, -0.5 * mouseY / windowHalfY, 0);
-      
-      // Supprimez ou commentez ces deux lignes pour que la position de la souris
-      // persiste entre les rendus:
-      // mouseX = 10000;
-      // mouseY = 10000;
+    positionUniforms['time'].value = now;
+    positionUniforms['delta'].value = delta;
+    velocityUniforms['time'].value = now;
+    velocityUniforms['delta'].value = delta;
+    birdUniforms['time'].value = now;
+    birdUniforms['delta'].value = delta;
 
-      gpuCompute.compute();
+    velocityUniforms['predator'].value.set(0.5 * mouseX / windowHalfX, -0.5 * mouseY / windowHalfY, 0);
+    
+    // Supprimez ou commentez ces deux lignes pour que la position de la souris
+    // persiste entre les rendus:
+    // mouseX = 10000;
+    // mouseY = 10000;
 
-      birdUniforms['texturePosition'].value = gpuCompute.getCurrentRenderTarget(positionVariable).texture;
-      birdUniforms['textureVelocity'].value = gpuCompute.getCurrentRenderTarget(velocityVariable).texture;
+    gpuCompute.compute();
 
-      renderer.render(scene, camera);
-    }
+    birdUniforms['texturePosition'].value = gpuCompute.getCurrentRenderTarget(positionVariable).texture;
+    birdUniforms['textureVelocity'].value = gpuCompute.getCurrentRenderTarget(velocityVariable).texture;
+
+    renderer.render(scene, camera);
+  }
+
+
+  useEffect(() => {
+    if(!containerRef.current) return 
 
     init()
   }, [containerRef])
